d50e338400498a977fae877b15e22872aec2d4c3
Raphael <67898112+RaphaelYim@users.noreply.github.com>
2025-03-21 12:30:27 -0600

    Update main.java
    comments

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,19 +3,19 @@ import com.phidget22.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner; //Required for Text Input
+import java.util.Scanner;
 import java.io.IOException;
 
 public class main {
 
     public static void main(String[] args) throws Exception {
 
-        List<StepperMotor> motorArray = new ArrayList<StepperMotor>();
-        for (int i=0;i!=7;i++) {
+        List<StepperMotor> motorArray = new ArrayList<StepperMotor>(); // ArrayList to store every created motor/
+        for (int i=0;i!=7;i++) { // Attempts to make motor instances
             try {
-                motorArray.add(new StepperMotor(i));
+                motorArray.add(new StepperMotor(i)); 
             } catch (Exception e) {
-                System.out.println("Failed to create Motor " + (i));
+                System.out.println("Failed to create Motor " + (i)); // error handling
                 e.printStackTrace();
             }
         }
